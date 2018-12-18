147fce838c181a25ef088013bfb64e23dee904a8
Jay Zawrotny <jayzawrotny@gmail.com>
2018-12-18 10:15:09 -0500

    Prepare for 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jayzawrotny/file-tree "0.1.0"
+(defproject jayzawrotny/file-tree "0.1.1-SNAPSHOT"
   :description "A Clojure library to return a breadth-first tree seq of files and directories."
   :url "https://github.com/jayzawrotny/file-tree"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
